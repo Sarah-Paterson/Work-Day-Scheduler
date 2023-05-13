863d4da3e43b4f92bb2db2c880a99ede2d234fa7
kitkatyourmom <sarahlridings@gmail.com>
2023-05-12 21:22:06 -0600

    add class change for time functions and date display function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+let currentDay = $('#currentDay');
+
+let hourTime = dayjs().format("H");
+
+let today = dayjs();
+
+// handle displaying the date
+function displayTime() {
+    var rightNow = today.format('MMM D, YYYY');
+    currentDay.text(rightNow);
+}
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -21,4 +34,136 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
-  
+
+
+
+// changing the class based on the time
+function hour9function() {
+    if(hourTime > 9) {
+        $("#hour-9").addClass("past");
+        console.log("past");
+    } else if (hourTime < 9) {
+        $("#hour-9").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-9").addClass("present");
+        console.log("present");
+    }
+}
+
+function hour10function() {
+    if(hourTime > 10) {
+        $("#hour-10").addClass("past");
+        console.log("past");
+    } else if (hourTime < 10) {
+        $("#hour-10").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-10").addClass("present");
+        console.log("present");
+    }
+}
+
+function hour11function() {
+    if(hourTime > 11) {
+        $("#hour-11").addClass("past");
+        console.log("past");
+    } else if (hourTime < 11) {
+        $("#hour-11").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-11").addClass("present");
+        console.log("present");
+    }
+}
+
+function hour12function() {
+    if(hourTime > 12) {
+        $("#hour-12").addClass("past");
+        console.log("past");
+    } else if (hourTime < 12) {
+        $("#hour-12").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-12").addClass("present");
+        console.log("present");
+    }
+}
+
+function hour13function() {
+    if(hourTime > 13) {
+        $("#hour-13").addClass("past");
+        console.log("past");
+    } else if (hourTime < 13) {
+        $("#hour-13").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-13").addClass("present");
+        console.log("present");
+    }
+}
+
+function hour14function() {
+    if(hourTime > 14) {
+        $("#hour-14").addClass("past");
+        console.log("past");
+    } else if (hourTime < 14) {
+        $("#hour-14").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-14").addClass("present");
+        console.log("present");
+    }
+}
+
+function hour15function() {
+    if(hourTime > 15) {
+        $("#hour-15").addClass("past");
+        console.log("past");
+    } else if (hourTime < 15) {
+        $("#hour-15").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-15").addClass("present");
+        console.log("present");
+    }
+}
+
+function hour16function() {
+    if(hourTime > 16) {
+        $("#hour-16").addClass("past");
+        console.log("past");
+    } else if (hourTime < 16) {
+        $("#hour-16").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-16").addClass("present");
+        console.log("present");
+    }
+}
+
+function hour17function() {
+    if(hourTime > 17) {
+        $("#hour-17").addClass("past");
+        console.log("past");
+    } else if (hourTime < 17) {
+        $("#hour-17").addClass("future");
+        console.log("future");
+    } else {
+        $("#hour-17").addClass("present");
+        console.log("present");
+    }
+}
+
+
+displayTime();
+
+hour9function();
+hour10function();
+hour11function();
+hour12function();
+hour13function();
+hour14function();
+hour15function();
+hour16function();
+hour17function();
