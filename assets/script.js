a78867268de6173dae6792162035b124d707c6b7
kitkatyourmom <sarahlridings@gmail.com>
2023-05-14 22:03:34 -0600

    copy save and get functions for all time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,213 +1,101 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 let currentDay = $('#currentDay');
-
 let hourTime = dayjs().format("H");
-
 let today = dayjs();
 
-// let hour9element = $('#hour-9');
-
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
 
+    // for loop with div class????????
 
-    
-
-    // Save button
-
-    // $("#hour-9-save").click(function(){
-    //     console.log("hour 9 save clicked");
-
-    //     let inputText = $('#hour-9-text');
-    //     let totalTasks = JSON.parse(localStorage.getItem("hour9-tasks")) || [];
-    
-    //     let saveText = inputText.value;
-    //     setSaveText(saveText);
-        
-    
-    //     function setSaveText(saveText) {
-    //         let tasks = {
-    //             inputText: saveText,
-    //         }
-            
-    //         totalTasks.push(tasks);
-    //         localStorage.setItem("hour9-tasks", JSON.stringify(totalTasks));
-         
-    //     }
-
-    //     inputText.textContent = totalTasks[i].inputText;
-
-
-    //   });
-
-    // // hour 9 set to storage
-    // localStorage.setItem(key,value);
-    // localStorage.getItem(key);
-
+    // Save buttons
     $("#saveBtn9").click(function(){
         // hour 9 get value
-        let value = $(".description").val();
+        let value = $("#text-area-9").val();
         $("#text-area-9").val(value);
         // hour 9 set to storage
         $(function () {
-            localStorage.setItem("#hour-9",value);
+            localStorage.setItem("hour-9",value);
         });
-        // hour 9 get from storage
-        // $(function () {
-        //     let storedValue = localStorage.getItem("#hour-9");
-        //     $("#text-area-9").val(storedValue);
-        // });
+    });
 
-    })
-    let storedValue = localStorage.getItem("#hour-9");
-    $("#text-area-9").val(storedValue);
+    $("#saveBtn10").click(function(){
+        // hour 10 get value
+        let value = $("#text-area-10").val();
+        $("#text-area-10").val(value);
+        // hour 10 set to storage
+        $(function () {
+            localStorage.setItem("hour-10",value);
+        });
+    });
 
-    console.log(storedValue);
-    console.log($("#text-area-9"));
-    
+    $("#saveBtn11").click(function(){
+        // hour 11 get value
+        let value = $("#text-area-11").val();
+        $("#text-area-11").val(value);
+        // hour 11 set to storage
+        $(function () {
+            localStorage.setItem("hour-11",value);
+        });
+    });
 
-    // $(".saveBtn").click(function(){
-    //     console.log("save clicked");
+    $("#saveBtn12").click(function(){
+        // hour 12 get value
+        let value = $("#text-area-12").val();
+        $("#text-area-12").val(value);
+        // hour 12 set to storage
+        $(function () {
+            localStorage.setItem("hour-12",value);
+        });
+    });
 
-        // $("#hour-9").submit(function() {
-        //     value9 = $("#hour-9").val();
-        //     $('#p').text(value);
-        // });
-        
-        // // hour 9 get value
-        // let value9 = $(".hour-9-text").val();
-        // $('.hour-9-text').text(value9);
-        // // hour 9 set to storage
-        // localStorage.setItem("#hour-9",value9);
-        // // hour 9 get from storage
-        // // localStorage.getItem("#hour-9");
-        
+    $("#saveBtn13").click(function(){
+        // hour 13 get value
+        let value = $("#text-area-13").val();
+        $("#text-area-13").val(value);
+        // hour 13 set to storage
+        $(function () {
+            localStorage.setItem("hour-13",value);
+        });
+    });
 
+    $("#saveBtn14").click(function(){
+        // hour 14 get value
+        let value = $("#text-area-14").val();
+        $("#text-area-14").val(value);
+        // hour 14 set to storage
+        $(function () {
+            localStorage.setItem("hour-14",value);
+        });
+    });
 
-        // let inputText = $('.description');
-        // let totalTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    
-        // let saveText = inputText.value;
-        // setSaveText(saveText);
-        
-        
-        // // this.appendChild(scoreCard);
-    
-        
-        
-        // function setSaveText(saveText) {
-        //     let tasks = {
-        //         inputText: saveText,
-        //     }
-            
-        //     totalTasks.push(tasks);
-        //     console.log('total tasks', totalTasks);
-        //     localStorage.setItem("tasks", JSON.stringify(totalTasks));
-         
-        // }
+    $("#saveBtn15").click(function(){
+        // hour 15 get value
+        let value = $("#text-area-15").val();
+        $("#text-area-15").val(value);
+        // hour 15 set to storage
+        $(function () {
+            localStorage.setItem("hour-15",value);
+        });
+    });
 
+    $("#saveBtn16").click(function(){
+        // hour 16 get value
+        let value = $("#text-area-16").val();
+        $("#text-area-16").val(value);
+        // hour 16 set to storage
+        $(function () {
+            localStorage.setItem("hour-16",value);
+        });
+    });
 
-
-    // });
-
-    // Reads projects from local storage and returns array of project objects.
-    // Returns an empty array ([]) if there aren't any projects.
-    // function readProjectsFromStorage() {
-    //     var projects = localStorage.getItem('projects');
-    //     if (projects) {
-    //     projects = JSON.parse(projects);
-    //     } else {
-    //     projects = [];
-    //     }
-    //     return projects;
-    // }
-    
-    // // Takes an array of projects and saves them in localStorage.
-    // function saveProjectsToStorage(projects) {
-    //     localStorage.setItem('projects', JSON.stringify(projects));
-    // }
-    
-    // // Gets project data from local storage and displays it
-    // function printProjectData() {
-    //     // clear current projects on the page
-    //     projectDisplayEl.empty();
-    
-    //     // get projects from localStorage
-    //     var projects = readProjectsFromStorage();
-
-    //     // Save the index of the project as a data-* attribute on the button. This
-    //     // will be used when removing the project from the array.
-    //     var deleteEl = $(
-    //         '<td><button class="btn btn-sm btn-delete-project" data-index="' +
-    //         i +
-    //         '">X</button></td>'
-    //     );
-
-    //     // append elements to DOM to display them
-    //     rowEl.append(nameEL, typeEl, dateEl, deleteEl);
-    //     projectDisplayEl.append(rowEl);
-    // }
-    // }
-        
-    // // Removes a project from local storage and prints the project data
-    // function handleDeleteProject() {
-    //     var projectIndex = parseInt($(this).attr('data-index'));
-    //     var projects = readProjectsFromStorage();
-    //     // remove project from the array
-    //     projects.splice(projectIndex, 1);
-    //     saveProjectsToStorage(projects);
-    
-    //     // print projects
-    //     printProjectData();
-    // }
-    
-    // // Adds a project to local storage and prints the project data
-    // function handleProjectFormSubmit(event) {
-    //     event.preventDefault();
-    
-    //     // read user input from the form
-    //     var projectName = projectNameInputEl.val().trim();
-    //     var projectType = projectTypeInputEl.val(); // don't need to trim select input
-    //     var projectDate = projectDateInputEl.val(); // yyyy-mm-dd format
-    
-    //     var newProject = {
-    //     name: projectName,
-    //     type: projectType,
-    //     date: projectDate,
-    //     };
-    
-    //     // add project to local storage
-    //     var projects = readProjectsFromStorage();
-    //     projects.push(newProject);
-    //     saveProjectsToStorage(projects);
-    
-    //     // print project data
-    //     printProjectData();
-    
-    //     // clear the form inputs
-    //     projectNameInputEl.val('');
-    //     projectTypeInputEl.val('');
-    //     projectDateInputEl.val('');
-    // }
-    
-    // projectFormEl.on('submit', handleProjectFormSubmit);
-    
-    // // Use jQuery event delegation to listen for clicks on dynamically added delete
-    // // buttons.
-    // projectDisplayEl.on('click', '.btn-delete-project', handleDeleteProject);
-    
-    // displayTime();
-    // setInterval(displayTime, 1000);
-    
-    // printProjectData();
+    $("#saveBtn17").click(function(){
+        // hour 17 get value
+        let value = $("#text-area-17").val();
+        $("#text-area-17").val(value);
+        // hour 17 set to storage
+        $(function () {
+            localStorage.setItem("hour-17",value);
+        });
+    });
 
     // changing block colors (class) based on the current time
     if(hourTime > 9) {
@@ -281,11 +169,34 @@ $(function () {
     } else {
         $("#hour-17").addClass("present");
     }
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
+
+    // retreve stored info
+    let storedValue9 = localStorage.getItem("hour-9");
+    $("#text-area-9").val(storedValue9);
+
+    let storedValue10 = localStorage.getItem("hour-10");
+    $("#text-area-10").val(storedValue10);
+
+    let storedValue11 = localStorage.getItem("hour-11");
+    $("#text-area-11").val(storedValue11);
+
+    let storedValue12 = localStorage.getItem("hour-12");
+    $("#text-area-12").val(storedValue12);
+
+    let storedValue13 = localStorage.getItem("hour-13");
+    $("#text-area-13").val(storedValue13);
+
+    let storedValue14 = localStorage.getItem("hour-14");
+    $("#text-area-14").val(storedValue14);
+
+    let storedValue15 = localStorage.getItem("hour-15");
+    $("#text-area-15").val(storedValue15);
+
+    let storedValue16 = localStorage.getItem("hour-16");
+    $("#text-area-16").val(storedValue16);
+
+    let storedValue17 = localStorage.getItem("hour-17");
+    $("#text-area-17").val(storedValue17);
 
     // the current date in the header of the page.
     function displayDate() {
@@ -295,8 +206,3 @@ $(function () {
 
     displayDate();
 });
-
-// $(window).load(function() {
-//     let storedValue = localStorage.getItem("#hour-9");
-//     $("#text-area-9").text(storedValue);
-// });
